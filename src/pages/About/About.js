019c91dbe5960b94be43e8fbace3bf871c1fe96d
inovatormatin <manojgeetparmar@gmail.com>
2022-05-26 22:38:00 +0530

    New: About us & contact us

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,80 @@
 import React from 'react'
 import './About.css'
+import a1 from '../../img/other/about1.jpg'
+import a2 from '../../img/other/about2.jpg'
 
 const About = () => {
   return (
     <div className='about'>
       <div className='aboutTitle'>
         <h2>About</h2>
+      </div>
+      <div className='aboutContent'>
+        <section className='acrs'>
+          <h3>Company Overview</h3>
+          <p>
+            The Company first online retailer to globally distribute a wide 
+            range of fashion, beauty and lifestyle products from Asia through 
+            their website example.com. Every season brings customers the 
+            latest trends along with thousands of items from brands across Asia. 
+            example.com now represents the best platform for brands in Asia 
+            to connect with customers worldwide.
+          </p>
+
+          <h3>Company Mission</h3>
+          <p>
+            The company brings the latest fashion trends to customers worldwide. 
+            Launched in July 2020, the example.com website leverages the success 
+            of a proven e-commerce platform and technology to deliver customers 
+            an exciting and unique destination for fashion, beauty, and lifestyle 
+            products.
+          </p>
+
+          <h3>Company Vision</h3>
+          <p>
+            To provide customers with an exciting shopping experience, superior 
+            service, and a fine selection of affordable, high-quality fashion, 
+            beauty, and lifestyle products.
+          </p>
+
+          <h3>Company Intro</h3>
+          <p>
+            To provide customers with an exciting shopping experience, superior 
+            service, and a fine selection of affordable, high-quality fashion, 
+            beauty, and lifestyle products.
+          </p>
+          <img src={a2} alt="about2img" />
+        </section>
+        <section className='acls'>
+          <img src={a1} alt="about1img" />
+          <h3>Company Profile</h3>
+          <p>
+            The Company first online retailer to globally distribute a wide 
+            range of fashion, beauty and lifestyle products from Asia through 
+            their website example.com. Every season brings customers the 
+            latest trends along with thousands of items from brands across Asia. 
+            example.com now represents the best platform for brands in Asia 
+            to connect with customers worldwide.
+          </p>
+
+          <h3>Mission 2030</h3>
+          <p>
+            The company brings the latest fashion trends to customers worldwide. 
+            Launched in July 2030, the example.com website leverages the success 
+            of a proven e-commerce platform and technology to deliver customers 
+            an exciting and unique destination for fashion, beauty, and lifestyle 
+            products.
+          </p>
+          <p>
+            offers express shipping on all orders. Shipments arrive within approximately 
+            3 working days from date of dispatch. Our ecommerce Phasellus luctus id 
+            turpis cursus. Donec at augue dictum dolor In tempor ultrices est,
+          </p>
+        </section>
+      </div>
+      <div className='needToKnow'>
+        <h1>Need to Know anything contact us</h1>
+        <button>Contact us</button>
       </div>
     </div>
   )
--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -7,6 +7,22 @@ const Contact = () => {
       <div className='contactTitle'>
         <h2>Contact</h2>
       </div>
+      <div className='contactContain'>
+        <form action="">
+          <input type="text" placeholder='Your Name*'/>
+          <input type="text" placeholder='Your email*'/>
+          <input type="text" placeholder='Subject*'/>
+          <textarea name="" id="" cols="30" rows="5" placeholder='Your message*' />
+          <button type='submit'>Submit</button>
+        </form>
+        <iframe 
+          title='title'
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d799.3767466570656!2d-119.79190944149457!3d36.734400094707375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8094609fa2bf2f13%3A0xca2c81043383dae1!2sFamily%20Fashions!5e0!3m2!1sen!2sin!4v1653573489461!5m2!1sen!2sin"   
+          loading="lazy" 
+          referrerpolicy="no-referrer-when-downgrade"
+          className='map'
+        />
+      </div>
     </div>
   )
 }
