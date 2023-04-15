1fe1d43af727dc79bd63abc9e059a9c3c949392a
inovatormatin <manojgeetparmar@gmail.com>
2023-04-15 21:22:41 +0530

    minor ui changes

--- src/components/HomeComp/HomeSlider/HomeSlider.js
+++ src/components/HomeComp/HomeSlider/HomeSlider.js
@@ -19,8 +19,49 @@ const HomeSlider = () => {
         className="mySwiper">
         <SwiperSlide>
           <div className='banner'>
-            <img className='bannerImg' src="https://res.cloudinary.com/inovatormatin/image/upload/v1653646901/eway/homeSlide/slider3_adqbhb.jpg" alt="banner1" />
+            <img className='bannerImg' src="https://res.cloudinary.com/inovatormatin/image/upload/v1681563758/eway/homeSlide/sllider_3_nl2uir.png" alt="banner1" />
+            <section className='offer leftSideOffer'>
+              <span style={{color: "#d23315"}}>REEBOK SPECIAL</span>
+              <h1>IMPOSSIBLE IS<br />NOTHING</h1>
+              <p>CHECK OUT SUMMER <br /> COLLECTION FIRST COME TO GET <br /> THE BEST!</p>
+              <div>
+                <Link to='./blog'>
+                  <button className='readMore' style={{backgroundColor: "#d23315"}}>READ MORE</button>
+                </Link>
+                <Link to='./shop'>
+                  <button className='showMore'>SHOP NOW</button>
+                </Link>
+              </div>
+            </section>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='banner'>
+            <img className='bannerImg' src="https://res.cloudinary.com/inovatormatin/image/upload/v1681565237/eway/homeSlide/sllider_4_p734lq.png" alt="banner4" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='banner'>
+            <img className='bannerImg' src="https://res.cloudinary.com/inovatormatin/image/upload/v1653646901/eway/homeSlide/slider3_adqbhb.jpg" alt="banner2" />
             <section className='offer rightSideOffer'>
+              <span>SPRING SPECIAL</span>
+              <h1>BECAUSE IMAGE<br />IS EVERYTHING</h1>
+              <p>CHECK OUT SUMMER <br /> COLLECTION FIRST COME TO GET <br /> THE BEST!</p>
+              <div>
+                <Link to='./blog'>
+                  <button className='readMore'>READ MORE</button>
+                </Link>
+                <Link to='./shop'>
+                  <button className='showMore'>SHOP NOW</button>
+                </Link>
+              </div>
+            </section>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='banner'>
+            <img className='bannerImg' src="https://res.cloudinary.com/inovatormatin/image/upload/v1653646901/eway/homeSlide/slider4_gqz07j.jpg" alt="banner3" />
+            <section className='offer leftSideOffer'>
               <span>SUMMER 30</span>
               <h1>NEW HOT <br />EVERYTHING</h1>
               <p>CHECK OUT SUMMER <br /> COLLECTION FIRST COME TO GET <br /> THE BEST!</p>
@@ -37,20 +78,7 @@ const HomeSlider = () => {
         </SwiperSlide>
         <SwiperSlide>
           <div className='banner'>
-            <img className='bannerImg' src="https://res.cloudinary.com/inovatormatin/image/upload/v1653646901/eway/homeSlide/slider4_gqz07j.jpg" alt="banner2" />
-            <section className='offer leftSideOffer'>
-              <span>SUMMER 30</span>
-              <h1>NEW HOT <br />EVERYTHING</h1>
-              <p>CHECK OUT SUMMER <br /> COLLECTION FIRST COME TO GET <br /> THE BEST!</p>
-              <div>
-                <Link to='./blog'>
-                  <button className='readMore'>READ MORE</button>
-                </Link>
-                <Link to='./shop'>
-                  <button className='showMore'>SHOP NOW</button>
-                </Link>
-              </div>
-            </section>
+            <img className='bannerImg' src="https://res.cloudinary.com/inovatormatin/image/upload/v1681566322/eway/homeSlide/sllider_5_ryxrfg.png" alt="banner5" />
           </div>
         </SwiperSlide>
       </Swiper>
