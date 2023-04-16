9e67a1568331ef789b83daf494dd0c2704c70760
inovatormatin <manojgeetparmar@gmail.com>
2023-04-16 14:30:42 +0530

    UI768PX: Responsivness for homepage

--- src/components/404/UnderDev.js
+++ src/components/404/UnderDev.js
@@ -8,7 +8,7 @@ const UnderDev = () => {
             <div className='underDev'>
                 <img src={img} alt="under development" />
                 <h1>Sorry !</h1>
-                <p>This website is not responsive for screen size with width less then 1100px. We are working on it, we launch new version soon.</p>
+                <p>This website is not responsive for screen size with width less then 767px. We are working on it, we launch new version soon.</p>
             </div>
         </div>
     )
