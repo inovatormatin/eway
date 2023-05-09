be8ec10284d24f1efd68d001a65743f37389d244
inovatormatin <manojgeetparmar@gmail.com>
2023-05-09 21:00:06 +0530

    UIMOB : Bottom naviagtion functionality issue fixed

--- src/components/BottomNavigation/BottomNavigation.js
+++ src/components/BottomNavigation/BottomNavigation.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import { GoHome } from "react-icons/go";
@@ -10,6 +11,12 @@ import "./bottomNavigationBar.css";
 
 const BottomNavigationbar = () => {
   const [value, setValue] = useState(0);
+  const navigate = useNavigate();
+  // to navigate page
+  const onClickHandler = (path) => {
+    let switchto = `${path}`
+    navigate(switchto)
+  }
 
   return (
     <div className="bottomNavigationBar">
@@ -22,12 +29,13 @@ const BottomNavigationbar = () => {
           }}
           className="bottomNavigationBarBox"
         >
-          <BottomNavigationAction label="HOME" icon={<GoHome />} />
-          <BottomNavigationAction label="SHOP" icon={<BiShoppingBag />} />
-          <BottomNavigationAction label="BLOG" icon={<AiOutlineRead />} />
+          <BottomNavigationAction label="HOME" icon={<GoHome />} onClick={() => onClickHandler("/")}/>
+          <BottomNavigationAction label="SHOP" icon={<BiShoppingBag />} onClick={() => onClickHandler("/shop")}/>
+          <BottomNavigationAction label="BLOG" icon={<AiOutlineRead />} onClick={() => onClickHandler("/blog")}/>
           <BottomNavigationAction
             label="CART"
             icon={<AiOutlineShoppingCart />}
+            onClick={() => onClickHandler("/mycart")}
           />
         </BottomNavigation>
       </Box>
